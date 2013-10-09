56c14cf8a3e7857c9591a24e8f0cc17385f60ea8
Alex Robinson <alex@rbnsn.me>
2013-10-09 21:56:51 +1000

    Initial Commit

---
+++ SegmentedContainerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SegmentedContainerDemo
+//
+//  Created by Alex Robinson on 9/10/2013.
+//  Copyright (c) 2013 Alex Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SegmentedContainerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SegmentedContainerDemo
+//
+//  Created by Alex Robinson on 9/10/2013.
+//  Copyright (c) 2013 Alex Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
